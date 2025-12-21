c5731d3c75ebace1f65c7bf83c1daef9d96559dd
yamantech <eishworacharya@gmail.com>
2025-12-21 08:45:30 +0545

    variables

---
+++ 01_javascriptFundamentals/01_variables.js
@@ -0,0 +1,8 @@
+ //  1.Variables
+let name = "Ishwor";     // Variable that can be reassigned
+console.log(`Name :${name}`);//here shown depricated beacuse of name is global property of window(web browser)
+const age = 23;        // Constant variable that cannot be reassigned
+console.log(`Age :${age}`);
+var isMarried = false;  // Older way to declare variables, function-scoped.
+console.log(`isMarried: ${isMarried}`);
+
