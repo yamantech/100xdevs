ead9a5cf4d12010613585d27959fcd9bc41b85fb
yamantech <eishworacharya@gmail.com>
2026-01-04 08:31:15 +0545

    function and block scope

--- 01_javascriptFundamentals/01_variables.js
+++ 01_javascriptFundamentals/01_variables.js
@@ -1,8 +1,30 @@
- //  1.Variables
-let name = "Ishwor";     // Variable that can be reassigned
-console.log(`Name :${name}`);//here shown depricated beacuse of name is global property of window(web browser)
-const age = 23;        // Constant variable that cannot be reassigned
-console.log(`Age :${age}`);
-var isMarried = false;  // Older way to declare variables, function-scoped.
-console.log(`isMarried: ${isMarried}`);
+//  //  1.Variables
+// let name = "Ishwor";     // let can be reassingment but never can be redefined (blocked scoped)
+// console.log(`Name :${name}`);
+// const age = 23;        // const can't never redeclare and reassign (blocked scoped)
+// console.log(`Age :${age}`);
 
+// var isMarried = false;  // not in use, can be redeclear and reassign(function-scoped).
+// console.log(`isMarried: ${isMarried}`);
+
+
+//Example 1  Var (function scope)
+/*
+function test(){
+    if(true){
+        var Name="Ishwor";
+    }
+    console.log(Name);
+}
+test()
+*/
+//Example 2 let (block scoped ({ })),same for const
+/*
+function test(){
+    if(true){
+        let Name="Ishwor";
+    }
+    console.log(Name);// error:ReferenceError: Name is not defined
+}
+test();
+*/
---
+++ 01_javascriptFundamentals/03_operators.js
@@ -0,0 +1,29 @@
+// 3. Operators
+/*
+Assignment Operator (=) assigns a value to a variable.
+
+Addition Operator (+) adds numeric values or concatenates strings.
+
+Multiplication Operator (*) multiplies numeric values.
+
+Comparison Operator (>) compares two values and returns a boolean (true or false).
+*/
+ 
+// 1. Artimatic operators
+let a, b;
+a = 2;
+b = 5;
+
+console.log(a + b);   // 7
+console.log(a - b);   // -3
+console.log(a * b);   // 10
+console.log(a / b);   // 0.4
+console.log(a % b);   // 2
+console.log(a++);     // prints 2, then a becomes 3
+console.log(++a);     // increments a to 4, prints 4 
+console.log(a--);     // prints 4, then a becomes 3 
+console.log(a);       // prints 3 
+console.log(--a);     // decrements a to 2, prints 2
+console.log(a ** a);  // 2 ** 2 = 4
+
+// 2. 
