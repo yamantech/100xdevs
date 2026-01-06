d004920c4cba243eb4aa299d15d2b618e3e7c3f3
yamantech <eishworacharya@gmail.com>
2026-01-06 09:59:40 +0545

    learn operators

--- 01_javascriptFundamentals/03_operators.js
+++ 01_javascriptFundamentals/03_operators.js
@@ -9,7 +9,8 @@ Multiplication Operator (*) multiplies numeric values.
 Comparison Operator (>) compares two values and returns a boolean (true or false).
 */
  
-// 1. Artimatic operators
+// // 1. Artimatic operators
+/*
 let a, b;
 a = 2;
 b = 5;
@@ -26,4 +27,32 @@ console.log(a);       // prints 3
 console.log(--a);     // decrements a to 2, prints 2
 console.log(a ** a);  // 2 ** 2 = 4
 
-// 2. 
+*/
+
+// // 2. Assignment Operators (=,+=,-=)
+/*
+a=3,b=4;
+console.log(a+=a);// a=a+a;
+console.log(a-=a);//a=a-a;
+*/
+
+// // 3. Comparision Operators(==,===,>=,>,<=,<)
+/*
+let c;
+a=2,b=3,c="2";
+console.log(a==b);//false 
+console.log(a===c);//false ( also checks the datatype)
+console.log(a<b);//true;
+
+*/
+
+// 4. Logical Operators (&& || ! )
+ 
+// with true and false
+/*
+let a=true,b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+
+*/
