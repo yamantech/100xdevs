7b6cfa3ba083df368e205a1293b2ab3895de9aba
yamantech <eishworacharya@gmail.com>
2026-01-14 17:22:38 +0545

    few class example

--- 03_promises/01_classes.js
+++ 03_promises/01_classes.js
@@ -13,8 +13,19 @@ class Rectangle{
         console.log(`The painted color is ${this.color}`) 
     }
 }
-const rect=new Rectangle(2,3,"blue");
+const rect=new Rectangle(2,3,"blue");// instance of Rectangle class
 const area=rect.area();
 console.log("Area without using method "+ rect.width*rect.height);
 console.log(area);
 rect.paint();
+
+//Some more classes
+//Date
+const now = new Date(); // Current date and time
+console.log(now.getDate());
+
+//Maps
+const map = new Map();
+map.set('name', 'Alice');
+map.set('age', 30);
+console.log(map.get('name'));
